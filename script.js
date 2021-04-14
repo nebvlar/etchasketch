be32479669d4de939cd4d72ed4dae75b46763176
nebvlar <nebvlar@gmail.com>
2021-04-14 14:30:48 -0700

    added event listener to fill the boxes on mouse over

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const container = document.querySelector("#container");
 
+
+
+
 for(var i = 0; i < 256; i++){
     grid = document.createElement('div');
-    grid.classList = "grid";
+    grid.className = "grid";
+    grid.id = "grid"
     container.appendChild(grid); 
+    
 }
+const gridPixel = document.querySelectorAll(".grid")
+
+gridPixel.forEach(gridPixel => {
+    gridPixel.addEventListener("mouseover", () => {
+        gridPixel.style.backgroundColor = "black"
+    })
+})
