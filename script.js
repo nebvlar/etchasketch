3db91316dbbd9b81b0402b81dad45d5a576d39e8
nebvlar <nebvlar@gmail.com>
2021-04-15 18:15:14 -0700

    slider now changes grid size - removed mouseover event to change grid to black at the moment

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
-const container = document.querySelector("#container");
-const resetButton = document.querySelector("#clear");
+const gridContainer = document.querySelector("#gridContainer");
+const clearButton = document.querySelector("#clear");
+let gridSize = 16;
 
-for(var i = 0; i < 256; i++){
-    grid = document.createElement('div');
-    grid.className = "grid";
-    grid.id = "grid"
-    container.appendChild(grid); 
-    
+window.addEventListener("load", setDefault(16))
+
+function setDefault(){
+    setGridSize(16);
+    fillGrid(16);
 }
-const gridPixel = document.querySelectorAll(".grid")
 
-gridPixel.forEach(gridPixel => {
-    gridPixel.addEventListener("mouseover", () => {
-        gridPixel.style.backgroundColor = "black"
-    })
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+let newSize = document.querySelector("#sizeRange")
+newSize.value = 16;
+let displaySize = document.querySelector("#sizeLabel");
+displaySize.textContent = newSize.value;
+newSize.addEventListener("mousemove", function(){
+    displaySize.textContent = newSize.value
 })
 
-resetButton.addEventListener("click", () =>{
-    window.location.reload()
+
+function setGridSize(gridSize){
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+}
+
+function fillGrid(gridSize){
+    let pixelAmount = gridSize * gridSize
+    for (let i = 0; i < pixelAmount; i++){
+        const gridPixel = document.createElement('div');
+        gridPixel.classList = "gridPixel";
+        gridPixel.style.backgroundColor = "white";
+        gridContainer.appendChild(gridPixel);
+    }
+}
+
+newSize.addEventListener('input',function(){
+    let gridSize = document.querySelector("#sizeRange").value;
+    removeAllChildNodes(gridContainer)
+    setGridSize(gridSize);
+    fillGrid(gridSize);
+
 })
